5e9950c0cbc588e95b0faa413c448b815d6ffde5
will oracions <Oracions.dev@gmail.com>
2021-11-16 13:22:53 +0100

    create expense model

--- client/src/app/models/besoins/besoin.model.ts
+++ client/src/app/models/besoins/besoin.model.ts
@@ -1,13 +1,28 @@
 import { BaseModel } from '@Models/base.model';
+import { ExpenseModel } from '@Models/expenses/expense.model';
 import { DeviseInterface } from '../devises/devise.model';
 
 export class BesoinModel extends BaseModel {
   constructor(
     public Name: string,
     public Desc: string,
-    public amount: number,
-    public devise: DeviseInterface
+    public Amount: number,
+    public Devise: DeviseInterface,
+    public Expense: ExpenseModel,
+    public Status: BesoinStatus,
+    public Priority: BesoinPriority
   ) {
     super();
   }
+}
+
+export enum BesoinStatus {
+  PENDING = 1,
+  CANCELED = 2,
+  COMPLETED = 3,
+}
+
+export enum BesoinPriority {
+  PRIMARY = 1,
+  SECONDARY = 2,
 }
--- client/src/app/models/expenses/expense.model.ts
+++ client/src/app/models/expenses/expense.model.ts
@@ -1,2 +1,13 @@
-export class Expense.Model {
+import { BaseModel } from '@Models/base.model';
+import { DeviseInterface } from '../devises/devise.model';
+
+export class ExpenseModel extends BaseModel {
+  constructor(
+    public Name: string,
+    public Desc: string,
+    public amount: number,
+    public devise: DeviseInterface
+  ) {
+    super();
+  }
 }
