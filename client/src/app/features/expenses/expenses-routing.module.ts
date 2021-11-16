3f4e7fa261f0e8bed3de917617579c038c532c24
will oracions <Oracions.dev@gmail.com>
2021-11-16 17:47:55 +0100

    integrating of besoin list template

--- client/src/app/features/expenses/expenses-routing.module.ts
+++ client/src/app/features/expenses/expenses-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpensesComponent } from './expenses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ExpensesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ExpensesRoutingModule { }
+export class ExpensesRoutingModule {}
